8eda688bed937bff35c4739bf6ec0366363b12f4
Mihai Balan <mihai.balan@gmail.com>
2018-09-08 01:25:36 +0300

    Initial pseudo-code-ish version

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 import boto3
+import peewee
 
 REGION_NAME = 'eu-central-1'
 VAULT_NAME = 'michou-photos'
 BUCKET_NAME = 'michou-photos-metadata'
 
 
-def main():
-    s3 = boto3.resource('s3', region_name=REGION_NAME)
+def list_local_folders(root_folder):
+    return []
 
-    if BUCKET_NAME not in map(lambda b: b.name, s3.buckets.all()):
-        print('Creating ', BUCKET_NAME)
-        s3.create_bucket(ACL='private', Bucket=BUCKET_NAME, CreateBucketConfiguration={
-            'LocationConstraint': REGION_NAME
-        })
 
-    glacier = boto3.resource('glacier', region_name=REGION_NAME)
+def hash_folder(folder_name):
+    return ''
 
-    glacier.create_vault(vaultName=VAULT_NAME)
 
-    for vault in glacier.vaults.all():
-        print(vault.name)
+def get_stored_folder_checksums(database, folder_name):
+    return []
+
+
+def retrieve_remote_database():
+    pass
+
+
+def pack_folder(folder_name):
+    return ''
+
+
+def upload_archive(archive_name):
+    pass
+
+
+def store_folder_version(database, folder_name, folder_checksum, archive_name):
+    pass
+
+
+def update_remote_database(database):
+    pass
+
+
+def main(root_folder):
+    database = retrieve_remote_database()
+    for folder_name in list_local_folders(root_folder):
+        folder_checksum = hash_folder(folder_name)
+        stored_folder_checksums = get_stored_folder_checksums(database, folder_name)
+        if folder_checksum not in stored_folder_checksums:
+            archive_name = pack_folder(folder_name)
+            upload_archive(archive_name)
+            store_folder_version(database, folder_name, folder_checksum, archive_name)
+    update_remote_database(database)
 
 
 if __name__ == '__main__':
